21bf2db5478acc2d03bb9752deeacfb20b3e4ed5
jakel181 <17256688+jakel181@users.noreply.github.com>
2020-02-18 13:13:05 -0500

    Update ScratchSig2.php

--- ScratchSig2.php
+++ ScratchSig2.php
@@ -44,7 +44,7 @@ function sigParserInit (Parser $parser) {
 
 function sigFetchProfile ($username) {
     // Fetch page
-    $data = file_get_contents('httsp://scratch.mit.edu/site-api/users/all/' . str_replace(' ', '_', $username) . '/');
+    $data = file_get_contents('https://scratch.mit.edu/site-api/users/all/' . str_replace(' ', '_', $username) . '/');
     $json = json_decode($data, $assoc=true);
     $pk = $json['user']['pk'];
     $image_url = "https://cdn.scratch.mit.edu/get_image/user/{$pk}_18x18.png";
